f1b1fcb1476e56830fac273c1646b84bce5d6e64
Marcelo Eugênio <123710944+CeloEug@users.noreply.github.com>
2024-05-30 19:48:44 -0300

    chore: configure bankaccount routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { theme } from './styles/Theme';
 import ContactDashboard from './pages/contact/ContactDashboard';
 import CreateContact from './pages/contact/CreateContact';
 import EditContact from './pages/contact/EditContact';
+import BankAccountDashboard from './pages/account/BankAccountDashboard';
+import CreateBankAccount from './pages/account/CreateBankAccount';
+import EditBankAccount from './pages/account/EditBankAccount';
 
 
 
@@ -24,6 +27,12 @@ function App() {
               <Route path='dashboard' element={<ContactDashboard />} />
               <Route path='create' element={<CreateContact />} />
               <Route path='edit/:id' element={<EditContact />} />
+            </Route>
+
+            <Route path='/bank_account'>
+              <Route path='dashboard' element={<BankAccountDashboard />} />
+              <Route path='create' element={<CreateBankAccount />} />
+              <Route path='edit/:id' element={<EditBankAccount />} />
             </Route>
           </Routes>
         </Layout>
