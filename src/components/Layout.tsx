9aac9bb1d28fc4fcea7109be24613cd609e18116
Marcelo Eugênio <123710944+CeloEug@users.noreply.github.com>
2024-06-03 19:52:35 -0300

    feat: add routes for the finance

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -41,7 +41,7 @@ function Layout(props: LayoutProps) {
           <Icon $src="/icon-products.svg" />
         </Link>
         
-        <Link to=''>
+        <Link to='/finance/dashboard'>
           <Icon $src="/icon-money.svg" />
         </Link>
       </SideBar>
