682e4ca677a8c8a72ff98d7886a6c15d35fbd5e7
Samir Buarque <samirbuarque@hotmail.com>
2024-06-07 08:36:42 -0300

    feat: create (create products form)

--- src/pages/products/CreateProducts.tsx
+++ src/pages/products/CreateProducts.tsx
@@ -1,12 +1,58 @@
 import { Container, PageHeader } from '../../styles/Global';
 import { Button } from '../../styles/components';
-import { Form, Label, SelectInput, TextInput } from '../../styles/pages/createContact';
+import { Form, Label, SelectInput, TextInput } from '../../styles/pages/createProducts';
 import { states } from '../../utils/auxData';
 import { handleSubmit } from '../../controllers/contactController';
 
 function CreateProducts() {
   return (
-    null
+    <main>
+      <Container>
+        <PageHeader>
+          <h1>Novo contato</h1>
+        </PageHeader>
+
+        <Form>
+          {/* First row */}
+          <Label className='full-column'>
+            Nome
+            <TextInput
+              placeholder='Ex: John Doe'
+            />
+          </Label>
+
+          {/* Second row */}
+          <Label className='full-column'>
+            Fornecedor
+            <SelectInput>
+
+            </SelectInput>
+          </Label>
+
+          {/* Third row */}
+          <Label>
+            Preço
+            <TextInput type='number'
+              placeholder='00-000.000'
+            />
+          </Label>
+
+          <Label>
+            Estoque
+            <TextInput 
+              placeholder='39'
+            />
+          </Label>
+
+          <Button
+            className='full-column'
+            onClick={handleSubmit}
+          >
+            Adicionar
+          </Button>
+        </Form>
+      </Container>
+    </main>
       
   )
 }
