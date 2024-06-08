975dd3934f2be440afb278f71e24153d4f1ba80a
Marcelo Eugênio <123710944+CeloEug@users.noreply.github.com>
2024-06-07 21:13:26 -0300

    feat: create login validation logic

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,52 @@
 import { TextInput } from '../../styles/pages/createContact';
-import { handleSubmit } from '../../controllers/contactController';
-import { BackgroundImg, Button, RightCard, LeftCard, Screen, Form, Img, Label, Container, ImgLabel } from '../../styles/pages/login'
+import { BackgroundImg, Button, RightCard, LeftCard, Screen, Form, Img, Label, Container, ImgLabel, MainLogin } from '../../styles/pages/login'
+import { setCookie } from 'nookies';
+import axios from 'axios';
+import { useState } from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [inputs, setInputs] = useState({ email: '', password: '' });
+  const { setAuthUser } = useAuth()
+  const navigate = useNavigate();
+  
+  const handleChange = (event: any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    try {
+      console.log(inputs);
+      const response = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/auth`, inputs);
+      
+      if (response.status === 200) {
+        const { client_id, access_token } = response.data;
+
+        setCookie(null, 'access_token', access_token, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/', 
+        });
+        setCookie(null, 'client_id', client_id, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/', 
+        });
+        setAuthUser(true)
+        navigate("/home")
+
+      } 
+    } catch (error: any) {
+      console.error('Error:', error.message);
+      alert(`${error}`)
+    }
+  };
+  
   return (
-    <main>
+    <MainLogin>
         <BackgroundImg src="https://as1.ftcdn.net/v2/jpg/01/22/20/44/1000_F_122204437_cka92jJ8XOTh4vrPu9fGq686qltKmNWE.jpg" alt="background" />      
         <Screen>
               <Container>
@@ -16,30 +58,37 @@ function Login() {
               </LeftCard>
 
               <RightCard>
-              <Form>
+              <Form onSubmit={handleSubmit}>
                 <Label>
                   Bem-vindo!
                 </Label>
                 
                 <TextInput
                   placeholder='Nome de Usuário'
+                  name="email" 
+                  value={inputs.email || ""} 
+                  onChange={handleChange}
                 />
               
                 <TextInput
-                  placeholder='Senha'
+                  placeholder='senha'
+                  type="password" 
+                  name="password" 
+                  value={inputs.password || ""} 
+                  onChange={handleChange}
                 />
               
                 <Button
                   className='full-column'
                   onClick={handleSubmit}
                 >
-                  Adicionar
+                  Login
                 </Button>
               </Form>
             </RightCard>
             </Container>
         </Screen>
-    </main>
+    </MainLogin>
   )
 }
 
