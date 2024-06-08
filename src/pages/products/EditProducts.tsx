c791f3b3270573be311eb7cdccdd9e829b068a02
Samir Buarque <samirbuarque@hotmail.com>
2024-06-08 09:50:40 -0300

    fix: editProducts page

--- src/pages/products/EditProducts.tsx
+++ src/pages/products/EditProducts.tsx
@@ -1,7 +1,7 @@
 import { Container, PageHeader } from '../../styles/Global';
 import { Button } from '../../styles/components';
 import { Form, Label, SelectInput, TextInput } from '../../styles/pages/createContact';
-import { states } from '../../utils/auxData';
+import { suppliers } from '../../utils/auxData';
 
 async function handleSubmit(e: any) {
   e.preventDefault();
@@ -10,7 +10,59 @@ async function handleSubmit(e: any) {
 
 function EditProducts() {
   return (
-    null
+    <main>
+      <Container>
+        <PageHeader>
+          <h1>Novo produto</h1>
+        </PageHeader>
+
+        <Form>
+          {/* First row */}
+          <Label className='full-column'>
+            Nome
+            <TextInput
+              placeholder='Ex: John Doe'
+            />
+          </Label>
+
+          {/* Second row */}
+          <Label className='full-column'>
+            Fornecedor
+            <SelectInput>
+              { 
+                suppliers.map((value, index) => (
+                  <option key={index} value={value}>
+                    {value}
+                  </option>
+                ))
+              }
+            </SelectInput>
+          </Label>
+
+          {/* Third row */}
+          <Label>
+            Preço
+            <TextInput type='number'
+              placeholder='00-000.000'
+            />
+          </Label>
+
+          <Label>
+            Estoque
+            <TextInput 
+              placeholder='39'
+            />
+          </Label>
+
+          <Button
+            className='full-column'
+            onClick={handleSubmit}
+          >
+            Adicionar
+          </Button>
+        </Form>
+      </Container>
+    </main>
   )
 }
 
